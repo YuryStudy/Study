da6a51ab3299aff2f4eeca5ea29a8ebdc079d4b1
Administrator <uran_second@mail.ru>
2023-03-31 12:23:20 +0500

    Модуль 14 задание 1

--- src/ru/javalang/module14/tasks/t01/Car.java
+++ src/ru/javalang/module14/tasks/t01/Car.java
@@ -3,15 +3,13 @@ package ru.javalang.module14.tasks.t01;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class Main {
+public class Car {
     public static void main(String[] args) {
-        Set<String> set = new TreeSet ();
+        Set<String> set = new TreeSet();
         set.add("VAZ");
         set.add("GAZ");
-        set.add("");
-        set.add("San Francisco");
-        set.add("Berling");
-        set.add("New York");
+        set.add("AZLK");
+        set.stream().sorted();
         System.out.println(set);
         for (Object element : set)
             System.out.println(element.toString());
